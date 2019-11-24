0ffd0565dc7f46eb7536772edaf4ab5a4a71b20a
gago55 <xgagik8@gmail.com>
2019-11-24 16:53:56 +0400

    Added Dropdown which value is kept in local state. Local state was created with hook useState

--- src/components/Cryptography.js
+++ src/components/Cryptography.js
@@ -6,14 +6,31 @@ import { FormControl, InputLabel, Select, MenuItem, Button } from '@material-ui/
 import { setInputTextAC } from '../redux/appReducer'
 
 const Cryptography = (props) => {
+    const [algorithm, setAlgorithm] = useState('des')
 
     const handleInputChange = event => {
         props.setInputText(event.target.value)
     }
 
+    const handleSelectChange = event => {
+        setAlgorithm(event.target.value)
+    }
+
     return (
         <div className={styles.mainDiv}>
             <TextField id="standard-basic" label="Input Text" value={props.inputText} onChange={handleInputChange} />
+            <FormControl >
+                <InputLabel id="demo-simple-select-label">Algorithm</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={algorithm}
+                    onChange={handleSelectChange}
+                >
+                    <MenuItem value={'des'}>DES</MenuItem>
+                    <MenuItem value={'gdes'}>GDES</MenuItem>
+                </Select>
+            </FormControl>
         </div>
     )
 }
