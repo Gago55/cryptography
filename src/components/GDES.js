6cff679c396e511c8452b256d18d3d2f547c44f7
gago55 <xgagik8@gmail.com>
2019-11-25 18:40:36 +0400

    Geneartion All 16 Keys

--- src/components/GDES.js
+++ src/components/GDES.js
@@ -17,6 +17,14 @@ class GDES {
         63, 55, 47, 39, 31, 23, 15, 7 , 62, 54, 46, 38, 30, 22, 
         14, 6 , 61, 53, 45, 37, 29, 21, 13, 5 , 28, 20, 12, 4
     ]
+    KeyShiftTable = [
+        1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1
+    ]
+    Key56To48Table = [
+        14,	17,	11,	24,	1,	5,	3,	28,	15,	6,	21,	10,	23,	19,	12,	4,
+        26,	8,	16,	7,	27,	20,	13,	2,	41,	52,	31,	37,	47,	55,	30,	40,
+        51,	45,	33,	48,	44,	49,	39,	56,	34,	53,	46,	42,	50,	36,	29,	32
+    ]
 
     binarText //Array which contain Binar Numbers of Text's Letters 
     initialText //Array after Initian Permutation
@@ -24,6 +32,7 @@ class GDES {
 
     key28_1 //Key after method key64to56()
     key28_2 //Key after method key64to56()
+    allKeys //16 keys for 16 rounds
 
     constructor(text, key) {
         this.text = text
@@ -36,10 +45,10 @@ class GDES {
         this.binarText = this.textToBin(this.text)
         this.initialText = this.initialPermutation()
         this.binarKey = this.textToBin(this.key)
-        
+
         this.key28_1 = this.key64to28(this.Key64To28Table1)
         this.key28_2 = this.key64to28(this.Key64To28Table2)
-        debugger
+        this.allKeys = this.generateAllKeys()
     }
 
     textToBin = text => {
@@ -70,6 +79,35 @@ class GDES {
             key28[i] = this.binarKey.join('').charAt(table[i]-1)
         }
         return key28
+    }
+    generateAllKeys = () =>{
+        let allKeys = new Array(16)
+        for (let i = 0 ; i < 16 ; i++){
+            this.key28_1 = this.leftShiftKey28(this.key28_1 , this.KeyShiftTable[i])
+            this.key28_2 = this.leftShiftKey28(this.key28_2 , this.KeyShiftTable[i])
+
+            let key56 = this.key28_1.join('') + this.key28_2.join('')
+            allKeys[i] = this.key56to48(key56)
+        }
+        return allKeys
+    }
+    key56to48 = (key56) =>{
+        let key48 = new Array(48)
+        for (let i = 0 ; i < 48 ; i++){
+            key48[i] = key56.charAt(this.Key56To48Table[i-1])
+        }
+        return key48
+    }
+
+    leftShiftKey28 = ( key , n) =>{
+        for (let i = 0 ; i < n ; i++){
+            let tmp = key[0]
+            for (let j = 0 ; j < key.length-1 ; j++){
+                key[j] = key[j+1]
+            }
+            key[27] = tmp
+        }
+        return key
     }
 }
 
