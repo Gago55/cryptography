2d94c10541997bb24e5e3163ebbe4538691d3712
gago55 <xgagik8@gmail.com>
2019-11-26 17:03:13 +0400

    Updates

---
+++ precache-manifest.1b9a421ae30d7b5798525216f3841728.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3689ed05ae9627c352424211028c9d8",
+    "url": "/cryptography/index.html"
+  },
+  {
+    "revision": "cbaaf5926be3a717335a",
+    "url": "/cryptography/static/css/main.27e1153a.chunk.css"
+  },
+  {
+    "revision": "f714728877e049b58255",
+    "url": "/cryptography/static/js/2.2699f7d3.chunk.js"
+  },
+  {
+    "revision": "cbaaf5926be3a717335a",
+    "url": "/cryptography/static/js/main.84133719.chunk.js"
+  },
+  {
+    "revision": "5147a08aa486dc28ea41",
+    "url": "/cryptography/static/js/runtime-main.18590f07.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31eb5e7234abee7323d8fd8245545a55.js"
+  "/cryptography/precache-manifest.1b9a421ae30d7b5798525216f3841728.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cryptography/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
