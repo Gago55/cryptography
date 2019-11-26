a5f252c5c3d0204637453282ec2725e2cad50d45
gago55 <xgagik8@gmail.com>
2019-11-26 17:01:03 +0400

    Updates

---
+++ precache-manifest.31eb5e7234abee7323d8fd8245545a55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1e4c728dfe19c14b81e4dce346ba70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b7a96285795a9675d05",
+    "url": "/static/css/main.27e1153a.chunk.css"
+  },
+  {
+    "revision": "f714728877e049b58255",
+    "url": "/static/js/2.2699f7d3.chunk.js"
+  },
+  {
+    "revision": "2b7a96285795a9675d05",
+    "url": "/static/js/main.f6bd71ea.chunk.js"
+  },
+  {
+    "revision": "62df7323b1a2475f849d",
+    "url": "/static/js/runtime-main.cda7dee7.js"
+  }
+]);
